a09031e851b3e4ffd5c4bf71fd2c6a2506d3c00b
kebaren <jantown@163.com>
2025-09-18 20:03:18 +0800

    grid layout

--- main.c
+++ main.c
@@ -1,34 +1,47 @@
 #include <gtk/gtk.h>
-static void print_hello(GtkWidget *widget, gpointer user_data)
+
+static void print_hello (GtkWidget *widget,gpointer   data)
 {
-    g_print("hello world!");
+  g_print ("Hello World\n");
 }
 
-static void activate(GtkApplication *app,gpointer user_data)
+static void activate (GtkApplication *app,gpointer        user_data)
 {
+  GtkWidget *window;
+  GtkWidget *button;
+  GtkWidget *grid;
 
-    GtkWidget *window;
-    GtkWidget *button;
-    window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window),"lemon");
-    gtk_window_set_default_size(GTK_WINDOW(window),600,800);
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
 
+  grid = gtk_grid_new();
+  gtk_window_set_child(GTK_WINDOW(window),grid);
+  button = gtk_button_new_with_label("Button1");
+  g_signal_connect(button,"clicked",G_CALLBACK(print_hello),NULL);
 
-    button = gtk_button_new_with_label("hello world!");
-    g_signal_connect(button,"clicked",G_CALLBACK(print_hello),NULL);
-    gtk_window_set_child(GTK_WINDOW(window),button);
-    gtk_window_present(GTK_WINDOW(window));
+  gtk_grid_attach(GTK_GRID(grid),button,0,0,1,1);
+
+  button = gtk_button_new_with_label("Button2");
+  g_signal_connect(button,"clicked",G_CALLBACK(print_hello),NULL);
+  gtk_grid_attach(GTK_GRID(grid),button,1,0,1,1);
+
+   button = gtk_button_new_with_label("quit");
+  g_signal_connect(button,"clicked",G_CALLBACK(gtk_window_destroy),window);
+  gtk_grid_attach(GTK_GRID(grid),button,0,1,2,1);
+
+  gtk_window_present (GTK_WINDOW (window));
 }
 
-int main(int argc, char **argv)
+int main (int    argc,char **argv)
 {
-    GtkApplication *app;
-    int status;
+  GtkApplication *app;
+  int status;
 
-    app = gtk_application_new("kebaren.lemon",G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect(app,"activate", G_CALLBACK(activate),NULL);
-    status = g_application_run(G_APPLICATION(app),argc,argv);
-    g_object_unref(app);
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
 
-    return status;
+  return status;
 }
